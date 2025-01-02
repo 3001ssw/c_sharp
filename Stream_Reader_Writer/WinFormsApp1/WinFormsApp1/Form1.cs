dd3b00e6545eb384f375a3bec196446f9a2cfa82
3001ssw <3001ssw@naver.com>
2025-01-02 23:01:15 +0900

    modified

--- Stream_Reader_Writer/WinFormsApp1/WinFormsApp1/Form1.cs
+++ Stream_Reader_Writer/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,10 +1,12 @@
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private string _file_name = ".\\test.txt";
         public Form1()
         {
             InitializeComponent();
@@ -19,23 +21,58 @@ namespace WinFormsApp1
 
         private void btnRead_Click(object sender, EventArgs e)
         {
+            try
+            {
+                StreamReader streamReader = new StreamReader(_file_name, //경로
+                                                             Encoding.UTF8); // 인코딩
+                if (streamReader.EndOfStream != true)
+                {
+                    string? strTemp = null;
+
+                    strTemp = streamReader.ReadLine();
+                    if (strTemp != null)
+                        tbText.Text = strTemp;
+
+                    strTemp = streamReader.ReadLine();
+                    if (strTemp != null)
+                        chkCheckOption.Checked = bool.Parse(strTemp);
+
+                    strTemp = streamReader.ReadLine();
+                    if (strTemp != null)
+                        cbCombo.Text = strTemp;
+                    strTemp = streamReader.ReadLine();
+                    if (strTemp != null)
+                        rdOption1.Checked = bool.Parse(strTemp);
+
+                    strTemp = streamReader.ReadLine();
+                    if (strTemp != null)
+                        rdOption2.Checked = bool.Parse(strTemp);
+
+                    strTemp = streamReader.ReadLine();
+                    if (strTemp != null)
+                        rdOption3.Checked = bool.Parse(strTemp);
+                }
+                streamReader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private void btnWrite_Click(object sender, EventArgs e)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine(tbText.Text);
-            stringBuilder.AppendLine(chkCheckOption.Checked.ToString());
-            stringBuilder.AppendLine(cbCombo.Text);
-            stringBuilder.AppendLine(rdOption1.Checked.ToString());
-            stringBuilder.AppendLine(rdOption2.Checked.ToString());
-            stringBuilder.AppendLine(rdOption3.Checked.ToString());
-            string strWrite = stringBuilder.ToString();
-            Debug.WriteLine(strWrite);
-
-            if (strWrite != string.Empty)
-            {
-            }
+            StreamWriter streamWriter = new StreamWriter(_file_name, // 경로
+                                                         false, //true: 뒤에 추가, false: 덮어 쓰기,
+                                                         Encoding.UTF8); // 인코딩
+            streamWriter.WriteLine(tbText.Text);
+            streamWriter.WriteLine(chkCheckOption.Checked.ToString());
+            streamWriter.WriteLine(cbCombo.Text);
+            streamWriter.WriteLine(rdOption1.Checked.ToString());
+            streamWriter.WriteLine(rdOption2.Checked.ToString());
+            streamWriter.WriteLine(rdOption3.Checked.ToString());
+            streamWriter.Close();
         }
     }
 }
