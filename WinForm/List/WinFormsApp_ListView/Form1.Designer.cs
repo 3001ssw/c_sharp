f33592ff9f4a34ee87fc67f2a5e565b0aa6c76f6
3001ssw <3001ssw@naver.com>
2025-01-06 16:08:05 +0900

    modifi list view code

--- WinForm/List/WinFormsApp_ListView/Form1.Designer.cs
+++ WinForm/List/WinFormsApp_ListView/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listView1 = new ListView();
+            cbViewMode = new ComboBox();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(33, 24);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(266, 122);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // cbViewMode
+            // 
+            cbViewMode.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbViewMode.FormattingEnabled = true;
+            cbViewMode.Location = new Point(351, 24);
+            cbViewMode.Name = "cbViewMode";
+            cbViewMode.Size = new Size(136, 23);
+            cbViewMode.TabIndex = 1;
+            cbViewMode.SelectedIndexChanged += cbViewMode_SelectedIndexChanged;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(34, 159);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "추가";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnAdd);
+            Controls.Add(cbViewMode);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListView listView1;
+        private ComboBox cbViewMode;
+        private Button btnAdd;
     }
 }
--- WinForm/List/WinFormsApp_ListView/Form1.cs
+++ WinForm/List/WinFormsApp_ListView/Form1.cs
@@ -1,3 +1,7 @@
+using System.Data;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace WinFormsApp_ListView
 {
     public partial class Form1 : Form
@@ -5,6 +9,62 @@ namespace WinFormsApp_ListView
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbViewMode.Items.AddRange(new string[] {
+                "View.LargeIcon",
+                "View.Details",
+                "View.SmallIcon",
+                "View.List",
+                "View.Tile" });
+            cbViewMode.SelectedIndex = 1;
+
+            listView1.Scrollable = true;
+
+            listView1.Columns.Add("Δ®·³1");
+            listView1.Columns.Add("Ό³Έν");
+            listView1.Columns.Add("Ό³Έν");
+        }
+
+        private void cbViewMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            View iStyle = View.Details;
+            switch (cbViewMode.Text)
+            {
+                case "View.LargeIcon":
+                    iStyle = View.LargeIcon;
+                    break;
+                case "View.Details":
+                    iStyle = View.Details;
+                    break;
+                case "View.SmallIcon":
+                    iStyle = View.SmallIcon;
+                    break;
+                case "View.List":
+                    iStyle = View.List;
+                    break;
+                case "View.Tile":
+                    iStyle = View.Tile;
+                    break;
+            }
+
+            listView1.View = iStyle;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listView1.BeginUpdate();
+            for (int i = 0; i < 3000; i++)
+            {
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Text = $"item{i}";
+                listViewItem.SubItems.Add($"SubItems1 {i}");
+                listViewItem.SubItems.Add($"SubItems2 {i}");
+                listView1.Items.Add(listViewItem);
+            }
+            listView1.EndUpdate();
         }
     }
 }
