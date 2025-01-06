cbc3842e8a8bbb89836e463f2b1d3a1520b9d857
3001ssw <3001ssw@naver.com>
2025-01-06 23:44:00 +0900

    modified list box code

--- WinForm/List/WinFormsApp_ListBox/Form1.cs
+++ WinForm/List/WinFormsApp_ListBox/Form1.cs
@@ -13,25 +13,27 @@ namespace WinFormsApp_ListBox
         private void Form1_Load(object sender, EventArgs e)
         {
             listBox1.ScrollAlwaysVisible = true; // 항상 스크롤바 보이게
+            //listBox1.SelectionMode = SelectionMode.One; // 단일 선택(기본값)
 
             listBox2.ScrollAlwaysVisible = true; // 항상 스크롤바 보이게
             //listBox2.SelectionMode = SelectionMode.None; // 선택 못함
-            //listBox2.SelectionMode = SelectionMode.One; // 1개 선택
-            //listBox2.SelectionMode = SelectionMode.MultiSimple; // 여러개 선택 가능. 단순 클릭으로 선택/선택해제 가능
-            listBox2.SelectionMode = SelectionMode.MultiExtended; // 여러개 선택 가능. Ctrl, Shift 키와 마우스 클릭으로 조합
+            //listBox2.SelectionMode = SelectionMode.One; // 단일 선택(기본값)
+            //listBox2.SelectionMode = SelectionMode.MultiSimple; // 다중 선택 가능. 단순 클릭으로 선택/선택해제 가능
+            listBox2.SelectionMode = SelectionMode.MultiExtended; // 다중 선택 가능. Ctrl, Shift 키와 마우스 클릭으로 조합
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            // 추가 버튼
             if (tbInput.Text == string.Empty)
                 return;
 
-            listBox1.Items.Add(tbInput.Text);
-
+            listBox1.Items.Add(tbInput.Text); // 텍스트 박스의 문자열을 리스트박스에 추가
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            // 삭제 버튼
             if (listBox1.SelectedItem == null)
                 return;
 
@@ -40,11 +42,13 @@ namespace WinFormsApp_ListBox
 
         private void btnDeleteAll_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear(); // 모두 지우기
+            // 모두 삭제 버튼
+            listBox1.Items.Clear();
         }
 
         private void btnSort_Click(object sender, EventArgs e)
         {
+            // 정렬 버튼
             listBox1.Sorted = !listBox1.Sorted;
             string strMsg = $"정렬 속성값이 [{listBox1.Sorted}]으로 설정되었습니다.";
             MessageBox.Show(strMsg);
@@ -60,7 +64,8 @@ namespace WinFormsApp_ListBox
 
         private void btnMultiAdd_Click(object sender, EventArgs e)
         {
-            listBox2.BeginUpdate(); // 화면 갱신을 일시적으로 중지
+            // 10000개 아이템 추가 버튼
+            //listBox2.BeginUpdate(); // 화면 갱신을 일시적으로 중지
 
             for (int i = 0; i < 10000; i++)
             {
@@ -68,11 +73,12 @@ namespace WinFormsApp_ListBox
                 listBox2.Items.Add(strItem);
             }
 
-            listBox2.EndUpdate(); // 화면 갱신을 다시 시작
+            //listBox2.EndUpdate(); // 화면 갱신을 다시 시작
         }
 
         private void btnMultiDelete_Click(object sender, EventArgs e)
         {
+            // 선택한 항목 삭제 버튼
             listBox2.BeginUpdate(); // 화면 갱신을 일시적으로 중지
 
             // 삭제는 역순으로 해야함
