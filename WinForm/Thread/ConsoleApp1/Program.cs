895e273b466f7212705088ed99edff4a36780be0
3001ssw <3001ssw@naver.com>
2025-01-09 07:43:47 +0900

    sample code 1

--- WinForm/Thread/ConsoleApp1/Program.cs
+++ WinForm/Thread/ConsoleApp1/Program.cs
@@ -7,41 +7,19 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Start Main");
-            Thread thread1 = new Thread(new ThreadStart(StartThread));
-            Thread thread2 = new Thread(new ParameterizedThreadStart(StartThreadParam));
-
-            thread1.Name = "Thread1";
-            thread2.Name = "Thread2";
-
+            Thread thread1 = new Thread(new ThreadStart(RunThread));
             thread1.Start();
-            thread2.Start(2);
-
-            thread1.Join();
-            thread2.Join();
 
             Console.WriteLine("Close Main");
         }
 
-        static void StartThread()
+        static void RunThread()
         {
-            Console.WriteLine("Start Thread: " + Thread.CurrentThread.Name);
+            Console.WriteLine("Start Thread" + Thread.CurrentThread.Name);
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Hello, World!");
                 Thread.Sleep(100);
-            }
-            Console.WriteLine("Close Thread");
-        }
-
-        static void StartThreadParam(object objParam)
-        {
-            int iParam = (int)objParam;
-            Console.WriteLine("Start Thread: " + Thread.CurrentThread.Name + ", param: " + iParam);
-
-            for (int i = 0; i < iParam; i++)
-            {
-                Console.WriteLine("print " + i);
-                Thread.Sleep(1000);
             }
             Console.WriteLine("Close Thread");
         }
