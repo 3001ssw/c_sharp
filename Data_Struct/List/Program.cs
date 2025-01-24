c2adee8c6968e35315caee57ff9ca00b0c57a756
3001ssw <3001ssw@naver.com>
2025-01-25 00:10:06 +0900

    list modified

--- Data_Struct/List/Program.cs
+++ Data_Struct/List/Program.cs
@@ -8,7 +8,7 @@ namespace List
         {
             List<CPerson> listPerson = new List<CPerson>(); // List 생성
 
-            // 추가
+            // 요소 추가
             listPerson.Add(new CPerson("김철수", 10));
             listPerson.Add(new CPerson("박민수", 15));
             listPerson.Add(new CPerson("이아영", 12));
@@ -18,10 +18,17 @@ namespace List
             listPerson.AddRange(people);
 
             // 조회
-            foreach (CPerson item in listPerson)
+            foreach (CPerson person in listPerson)
             {
-                Console.WriteLine(item.ToString());
+                Console.WriteLine(person.ToString());
             }
+            Console.WriteLine();
+
+            // 각 요소에 대해 지정된 작업 수행
+            listPerson.ForEach(person =>
+            {
+                Console.WriteLine(person.ToString());
+            });
             Console.WriteLine();
 
             // 총 개수
@@ -30,16 +37,16 @@ namespace List
             Console.WriteLine();
 
             // 찾기
-            int iIndex = listPerson.FindIndex(person => person.NAME == "이아영");
+            int iIndex = listPerson.FindIndex(element => element.NAME == "이아영");
             Console.WriteLine($"Find Index: {iIndex}");
             Console.WriteLine();
 
             // 인덱스로 요소 가져오기
             try
             {
-                CPerson cFind1 = listPerson[10]; // 인덱스 범위 밖이면 ArgumentOutOfRangeException 발생
-                //CPerson cFind1 = listPerson[iIndex]; // 인덱스 범위 밖이면 ArgumentOutOfRangeException 발생
-                Console.WriteLine($"Index: {iIndex}, Find: [{cFind1.ToString()}]");
+                //CPerson cfind = listPerson[iIndex];
+                CPerson cFind = listPerson[10]; // 인덱스 범위 밖이면 ArgumentOutOfRangeException 발생
+                Console.WriteLine($"Index: {iIndex}, Find: [{cFind.ToString()}]");
             }
             catch (ArgumentOutOfRangeException ex)
             {
@@ -47,34 +54,55 @@ namespace List
             }
             Console.WriteLine();
 
-            CPerson cFind2 = listPerson.ElementAt(iIndex);
-            Console.WriteLine($"Index: {iIndex}, Find: [{cFind2.ToString()}]");
+            try
+            {
+                CPerson cFind = listPerson.ElementAt(iIndex);
+                //CPerson cfind = listPerson.ElementAt(10); // 인덱스 범위 밖이면 ArgumentOutOfRangeException 발생
+                Console.WriteLine($"Index: {iIndex}, Find: [{cFind.ToString()}]");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.WriteLine();
 
             // 요소로 인덱스 가져오기
-            int iIndexByElement = listPerson.IndexOf(cFind2);
-            Console.WriteLine($"element: [{cFind2.ToString()}], index: {iIndexByElement}");
+            CPerson element = listPerson.ElementAt(iIndex);
+            int iIndexByElement = listPerson.IndexOf(element);
+            Console.WriteLine($"elementment: [{element.ToString()}], index: {iIndexByElement}");
+            Console.WriteLine();
+
+            // 요소 있는지 여부 확인
+            bool bContain = listPerson.Contains(element);
+            Console.WriteLine($"is contain : {bContain}");
             Console.WriteLine();
 
             // 정렬
             Console.WriteLine("======= 이름으로 정렬 =======");
-            listPerson.Sort((x, y) => x.NAME.CompareTo(y.NAME)); // 이름 기준
-            foreach (CPerson item in listPerson)
+            listPerson.Sort((x, y) => x.NAME.CompareTo(y.NAME)); // 이름 기준 정렬
+            foreach (CPerson person in listPerson)
             {
-                Console.WriteLine(item.ToString());
+                Console.WriteLine(person.ToString());
             }
             Console.WriteLine("============================");
             Console.WriteLine();
 
             Console.WriteLine("======= 나이로 정렬 ========");
-            listPerson.Sort((x, y) => x.AGE.CompareTo(y.AGE)); // 나이 기준
-            foreach (CPerson item in listPerson)
+            listPerson.Sort((x, y) => x.AGE.CompareTo(y.AGE)); // 나이 기준 정렬
+            foreach (CPerson person in listPerson)
             {
-                Console.WriteLine(item.ToString());
+                Console.WriteLine(person.ToString());
             }
             Console.WriteLine("============================");
+            Console.WriteLine();
 
-            // 삭제
+            // 요소로 삭제
+            listPerson.Remove(element);
+
+            // 인덱스로 삭제
+            listPerson.RemoveAt(0);
+
+            // 모든 요소 삭제
             listPerson.Clear();
         }
     }
