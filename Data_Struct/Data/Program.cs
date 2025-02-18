559234750d8e40a5f734b55862250f06c6aaed61
3001ssw <3001ssw@naver.com>
2025-02-19 00:21:22 +0900

    modify data

---
+++ Data_Struct/Data/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+
+namespace Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DataTable 생성
+            DataTable table = new DataTable("Fruit");
+
+            // 컬럼 추가
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Price", typeof(int));
+
+            // 행 추가
+            table.Rows.Add("Apple", 1200);
+            table.Rows.Add("Banana", 1000);
+
+            DataRow rowCherry = table.NewRow();
+            rowCherry["Name"] = "Cherry";
+            rowCherry["Price"] = 2500;
+            table.Rows.Add(rowCherry);
+
+
+            // 데이터 출력
+            foreach (DataRow row in table.Rows)
+            {
+                Console.WriteLine($"{row["Name"]}, {row["Price"]}");
+            }
+        }
+    }
+}
