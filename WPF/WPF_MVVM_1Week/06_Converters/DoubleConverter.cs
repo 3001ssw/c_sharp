b913d6fb99f5aa127845faa403e24ee85d7804fd
3001ssw <3001ssw@naver.com>
2025-03-14 17:50:58 +0900

    databinding, converters

---
+++ WPF/WPF_MVVM_1Week/06_Converters/DoubleConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _06_Converters
+{
+    class DoubleConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string strValue)
+            {
+                if (int.TryParse(strValue, out int number))
+                    return number * 2;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string strValue)
+            {
+                if (int.TryParse(strValue, out int number))
+                    return number / 2;
+            }
+            return value;
+        }
+    }
+}
