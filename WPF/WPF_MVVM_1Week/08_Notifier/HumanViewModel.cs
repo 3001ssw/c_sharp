911deff48647a57fd97c5c5cf007131a2a024f40
3001ssw <3001ssw@naver.com>
2025-04-16 17:11:05 +0900

    modify nofifier

---
+++ WPF/WPF_MVVM_1Week/08_Notifier/HumanViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Notifier
+{
+    public class HumanViewModel : Notifier
+    {
+        public Person Humans { get; set; } = new Person(); // ObservableCollection<Human>
+
+        private Human _selectedHuman;
+        public Human SelectedHuman
+        {
+            get => _selectedHuman;
+            set
+            {
+                _selectedHuman = value;
+                OnPropertyChanged(nameof(SelectedHuman));
+            }
+        }
+
+        public HumanViewModel()
+        {
+            SelectedHuman = Humans.FirstOrDefault(); // 초기 선택값
+        }
+    }
+
+}
--- WPF/WPF_MVVM_1Week/08_Notifier/MainWindow.xaml.cs
+++ WPF/WPF_MVVM_1Week/08_Notifier/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace _08_Notifier
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new HumanViewModel();
         }
     }
 }
