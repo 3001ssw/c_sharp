65c4e1ff9523f041ee2ef638ed212018dc1b43df
3001ssw <3001ssw@naver.com>
2025-06-26 12:52:47 +0900

    modified mvvm

---
+++ WPF/Simple_WfpApp/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.ViewModels;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowInfo()
+        {
+
+        }
+
+        private void ShowInfo(object sender, RoutedEventArgs e)
+        {
+            // UserControl에서 ViewModel 가져오기
+            PersonViewModel vm = PersonViewControl.DataContext as PersonViewModel;
+            if (vm != null)
+            {
+                MessageBox.Show($"이름: {vm.Name}\n나이: {vm.Age}", "Person 정보");
+            }
+        }
+    }
+}
---
+++ WPF/Simple_WfpApp/WpfApp4/ViewModels/PersonViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp4.Models;
+
+namespace WpfApp4.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+
+        public PersonViewModel()
+        {
+            _person = new Person { Name = "홍길동", Age = 30 };
+        }
+
+        public string Name
+        {
+            get => _person.Name;
+            set
+            {
+                if (_person.Name != value)
+                {
+                    _person.Name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => _person.Age;
+            set
+            {
+                if (_person.Age != value)
+                {
+                    _person.Age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+
+}
