ef2ee5a343e2fec401c9d41e6029069375aa42f8
3001ssw <3001ssw@naver.com>
2025-09-27 13:45:25 +0900

    modified

---
+++ WPF/WPF_Basic/WpfTabControl/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace WpfTabControl
+{
+    public class MainWindowViewModel : Notifier
+    {
+        public ObservableCollection<TabItemViewModel> Tabs { get; }
+
+        public MainWindowViewModel()
+        {
+            Tabs = new ObservableCollection<TabItemViewModel>
+            {
+                new TabItemViewModel
+                {
+                    Header = "첫번째 탭",
+                    Content = new Views.MyControl()   // UserControl 직접 할당
+                },
+                new TabItemViewModel
+                {
+                    Header = "두번째 탭",
+                    Content = new Views.MyControl()   // 또 다른 UserControl
+                }
+            };
+        }
+    }
+}
---
+++ WPF/WPF_Basic/WpfTabControl/TabItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfTabControl
+{
+    public class TabItemViewModel
+    {
+        public string Header { get; set; } = "";   // 탭 제목
+        public object Content { get; set; } = null;  // 실제 보여줄 UserControl or ViewModel
+    }
+}
