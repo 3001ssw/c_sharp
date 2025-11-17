a0d13f9cebac327c85a4d009ef87a473e57b01a0
3001ssw <3001ssw@naver.com>
2025-11-17 23:49:33 +0900

    scrollviewer

---
+++ WPF/Simple_WfpApp/Messenger01/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger01
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _count;
+
+        // 자동 ICommand 생성
+        [RelayCommand]
+        private void CountCommand()
+        {
+            Count++;
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
