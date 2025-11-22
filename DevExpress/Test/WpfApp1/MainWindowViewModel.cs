cf03f4785977c26cf2209c3ae21b0ae800863ad3
3001ssw <3001ssw@naver.com>
2025-11-22 19:47:29 +0900

    poco, devexpress mvvm

---
+++ DevExpress/Test/WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Message
+        {
+            get => GetProperty(() => Message);
+            set => SetProperty(() => Message, value);
+        }
+
+        public ICommand ClickCommand { get; }
+        public ICommand ClickParameterCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            ClickCommand = new DelegateCommand(OnClickCommand, OnCanClickCommand);
+            ClickParameterCommand = new DelegateCommand<string>(OnClickParameterCommand, OnCanClickParameterCommand);
+        }
+
+        private void OnClickCommand()
+        {
+            MessageBox.Show($"Message: {Message}");
+        }
+
+        private bool OnCanClickCommand()
+        {
+            if (string.IsNullOrEmpty(Message))
+                return false;
+            else
+                return true;
+        }
+
+        private void OnClickParameterCommand(string message)
+        {
+            MessageBox.Show($"Message: {message}");
+        }
+
+        private bool OnCanClickParameterCommand(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ DevExpress/Test/WpfApp2/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    [POCOViewModel]
+    public class MainWindowViewModel
+    {
+        public virtual string Message { get; set; } = "";
+
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public void Click()
+        {
+            MessageBox.Show($"Message: {Message}");
+        }
+
+        public bool CanClick()
+        {
+            if (string.IsNullOrEmpty(Message))
+                return false;
+            else
+                return true;
+        }
+
+        public void ClickParameter(string message)
+        {
+            MessageBox.Show($"Message: {message}");
+        }
+
+        public bool CanClickParameter(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return false;
+            else
+                return true;
+        }
+    }
+}
