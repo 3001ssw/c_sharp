d6353fecf83d098dcf6167d66015f0f27823179a
3001ssw <3001ssw@naver.com>
2025-11-24 00:14:59 +0900

    add lock

--- basic/_lock/Program.cs
+++ basic/_lock/Program.cs
@@ -2,19 +2,10 @@
 {
     internal class Program
     {
-        static object lock_obj = new object();
-        static int number = 0;
-
         static void Main(string[] args)
         {
-            Thread thread1 = new Thread(() => Run())
-            {
-                Name = "Thread1",
-            };
-            Thread thread2 = new Thread(() => Run())
-            {
-                Name = "Thread2",
-            };
+            Thread thread1 = new Thread(() => Run());
+            Thread thread2 = new Thread(() => Run());
 
             thread1.Start();
             thread2.Start();
@@ -23,11 +14,14 @@
             thread2.Join();
         }
 
+        static object lock_obj = new object(); // lock 개체
+        static int number = 0; // 두 개의 쓰레드에 공유된 자원
+
         static void Run()
         {
             for (int i = 0; i < 100; i++)
             {
-                //lock(lock_obj)
+                lock (lock_obj) // lock_obj를 기준으로 임계영역 설정
                 {
                     int temp = number;
                     Console.WriteLine($"현재 숫자 : [{number}]");
@@ -41,7 +35,7 @@
                         Console.WriteLine($"???????? [{number}] ????????");
 
                     Thread.Sleep(10);
-                }
+                } // lock 블록 종료.
             }
         }
     }
