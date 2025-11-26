3033d1f16dfb7c8871347cccc2e8f3c19c56f800
3001ssw <3001ssw@naver.com>
2025-11-26 21:33:11 +0900

    add guid project

---
+++ basic/_guid/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _guid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strGuid = Guid.NewGuid().ToString();
+            Console.WriteLine($"GUID: {strGuid}");
+
+            if (Guid.TryParse(strGuid, out Guid guid))
+            {
+                Console.WriteLine($"GUID: {guid}");
+            }
+        }
+    }
+}
