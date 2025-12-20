2bb68a749f158d2c06443e0e76ccb9fd17f3b4e2
3001ssw <3001ssw@naver.com>
2025-12-20 21:45:50 +0900

    behavior

---
+++ WPF/WPF_Basic/WpfBehavior/behavior/TextBoxBehavior.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfBehavior.behavior
+{
+    public class TextBoxBehavior : Behavior<TextBox>
+    {
+        public TextBoxBehavior()
+        {
+            
+        }
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            // 키보드 포커스를 받았을 때
+            AssociatedObject.GotKeyboardFocus += OnGotKeyboardFocus;
+            // 마우스 클릭으로 포커스를 받았을 때를 대비
+            AssociatedObject.PreviewMouseLeftButtonDown += OnPreviewMouseLeftButtonDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.GotKeyboardFocus -= OnGotKeyboardFocus;
+            AssociatedObject.PreviewMouseLeftButtonDown -= OnPreviewMouseLeftButtonDown;
+        }
+
+        private void OnGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+        }
+
+        private void OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // 이미 포커스가 있는 상태에서 클릭한 건 무시
+            if (!AssociatedObject.IsKeyboardFocusWithin)
+            {
+                AssociatedObject.Focus();
+                e.Handled = true; // 클릭 이벤트가 텍스트 커서 위치를 바꿔버리는 걸 방지
+            }
+        }
+    }
+}
