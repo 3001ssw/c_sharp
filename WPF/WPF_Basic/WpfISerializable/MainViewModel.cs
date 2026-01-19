becfa393c3ddedef695a1b57b69952ff41ae506b
3001ssw <3001ssw@naver.com>
2026-01-20 00:41:00 +0900

    serializable

---
+++ WPF/WPF_Basic/WpfISerializable/MainViewModel.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Win32;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfISerializable
+{
+    public class MainViewModel : BindableBase
+    {
+        #region fields, properties
+        private ObservableCollection<UserInfo> uers = new ObservableCollection<UserInfo>();
+        public ObservableCollection<UserInfo> Users { get => uers; set => SetProperty(ref uers, value); }
+        #endregion
+
+        #region commands
+        public DelegateCommand XmlSaveCommand { get; private set; }
+        public DelegateCommand XmlLoadCommand { get; private set; }
+        public DelegateCommand JsonSaveCommand { get; private set; }
+        public DelegateCommand JsonLoadCommand { get; private set; }
+        #endregion
+
+
+        public MainViewModel()
+        {
+            XmlSaveCommand = new DelegateCommand(OnXmlSave, CanXmlSave);
+            XmlLoadCommand = new DelegateCommand(OnXmlLoad, CanXmlLoad);
+
+            JsonSaveCommand = new DelegateCommand(OnJsonSave, CanJsonSave);
+            JsonLoadCommand = new DelegateCommand(OnJsonLoad, CanJsonLoad);
+        }
+
+        private void OnXmlSave()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+            saveFileDialog.DefaultExt = "xml";
+            saveFileDialog.FileName = "users.xml";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = saveFileDialog.FileName;
+                    XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<UserInfo>));
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        serializer.Serialize(writer, Users);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private bool CanXmlSave()
+        {
+            return true;
+        }
+
+        private void OnXmlLoad()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = openFileDialog.FileName;
+                    XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<UserInfo>));
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        var data = (ObservableCollection<UserInfo>)serializer.Deserialize(reader);
+                        Users = data;
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+        private bool CanXmlLoad()
+        {
+            return true;
+        }
+
+        private void OnJsonSave()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*";
+            saveFileDialog.DefaultExt = "json";
+            saveFileDialog.FileName = "users.json";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = saveFileDialog.FileName;
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                    };
+
+                    string jsonString = JsonSerializer.Serialize(Users, options);
+                    File.WriteAllText(filePath, jsonString);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+        private bool CanJsonSave()
+        {
+            return true;
+        }
+
+        private void OnJsonLoad()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*";
+            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = openFileDialog.FileName;
+                    string jsonString = File.ReadAllText(filePath);
+                    var data = JsonSerializer.Deserialize<ObservableCollection<UserInfo>>(jsonString);
+                    if (data != null)
+                    {
+                        Users = data;
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+        private bool CanJsonLoad()
+        {
+            return true;
+        }
+    }
+}
---
+++ WPF/WPF_Basic/WpfISerializable/UserInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace WpfISerializable
+{
+    [Serializable]
+    public class UserInfo : BindableBase
+    {
+        #region fields, properties
+        private string name = "";
+        public string Name { get => name; set => SetProperty(ref name, value); }
+
+        private int age = 0;
+        public int Age { get => age; set => SetProperty(ref age, value); }
+
+        private string gender = "남";
+        public string Gender { get => gender; set => SetProperty(ref gender, value); }
+
+        #endregion
+
+        public UserInfo()
+        {
+            
+        }
+    }
+}
