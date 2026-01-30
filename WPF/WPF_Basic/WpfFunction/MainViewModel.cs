e2f082de86694ce790b0d35a7f352267773b9086
3001ssw <3001ssw@naver.com>
2026-01-30 14:42:13 +0900

    add wpf function

---
+++ WPF/WPF_Basic/WpfFunction/MainViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfFunction;
+
+namespace WpfFunction
+{
+    public class MainViewModel : BindableBase
+    {
+        #region fields, properties
+
+        private ObservableCollection<Student> students = new ObservableCollection<Student>();
+        public ObservableCollection<Student> Students { get => students; set => SetProperty(ref students, value); }
+
+        private ObservableCollection<Student> results = new ObservableCollection<Student>();
+        public ObservableCollection<Student> Results { get => results; set => SetProperty(ref results, value); }
+
+        private int score = 80;
+        public int Score { get => score; set => SetProperty(ref score, value); }
+
+        public Func<Student, bool> GetPass { get => (s) => s.Score >= Score; }
+        public Func<Student, bool> GetFail { get => (s) => s.Score < Score; }
+
+        #endregion
+
+        #region commands
+
+        public DelegateCommand GetPassCommand { get; private set; }
+        public DelegateCommand GetFailCommand { get; private set; }
+
+        #endregion
+
+        #region command methods
+
+        private void OnGetPass()
+        {
+            var passStudents = GetList(GetPass);
+
+            Results.Clear();
+            foreach (var student in passStudents)
+            {
+                Results.Add(student);
+            }
+        }
+
+        private bool CanGetPass()
+        {
+            return true;
+        }
+
+        private void OnGetFail()
+        {
+            var passStudents = GetList(GetFail);
+
+            Results.Clear();
+            foreach (var student in passStudents)
+            {
+                Results.Add(student);
+            }
+        }
+
+        private bool CanGetFail()
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region functions
+
+        IEnumerable<Student> GetList(Func<Student, bool> func)
+        {
+            return Students.Where(func).ToList();
+        }
+
+        #endregion
+
+
+
+        #region constructor
+        public MainViewModel()
+        {
+            GetPassCommand = new DelegateCommand(OnGetPass, CanGetPass);
+            GetFailCommand = new DelegateCommand(OnGetFail, CanGetFail);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Students.Add(new Student());
+            }
+        }
+        #endregion
+    }
+}
---
+++ WPF/WPF_Basic/WpfFunction/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfFunction
+{
+    public class Student : BindableBase
+    {
+        private Guid id = Guid.NewGuid();
+        public Guid ID { get => id; set => SetProperty(ref id, value); }
+
+        private string name = "";
+        public string Name { get => name; set => SetProperty(ref name, value); }
+
+        private int score = 0;
+        public int Score { get => score; set => SetProperty(ref score, value); }
+
+        public Student()
+        {
+            MakeRandomNameNScore();
+        }
+
+        private void MakeRandomNameNScore()
+        {
+            Random rand = new Random();
+
+            string[] _lastNames = { "김", "이", "박", "최", "정", "강", "조", "윤", "장", "임", "한", "오", "서" }; // 성
+            string[] _nameChars = {"서", "준", "도", "윤", "하", "예", "지", "민", "현", "우", "은", "빈", "나",
+                "건", "유", "진", "슬", "율", "주", "연", "아", "태", "정", "훈", "승", "채", "가", "선", "리", "안" };// 이름
+
+            string lastName = _lastNames[rand.Next(0, _lastNames.Length)]; // 성
+            string char1 = _nameChars[rand.Next(0, _nameChars.Length)]; // 이름1
+            string char2 = _nameChars[rand.Next(0, _nameChars.Length)]; // 이름2
+
+            Name = $"{lastName}{char1}{char2}";
+            Score = rand.Next(0, 101);
+        }
+    }
+}
