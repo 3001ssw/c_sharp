ae9dc2ee43b5eb692b032476c31b99b2a7a87f2a
3001ssw <3001ssw@naver.com>
2026-02-04 00:01:41 +0900

    add radiobutton

---
+++ WPF/WPF_Basic/WpfRadioButton/FruitEnumToBoolConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfRadioButton
+{
+    public class FruitEnumToBoolConverter : IValueConverter
+    {
+        public enum FruitType { Apple, Orange, Grape };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is FruitType type)
+            {
+                if (type.ToString() == parameter.ToString())
+                    return true;
+                else
+                    return false;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool check)
+            {
+                if (check)
+                    return parameter;
+                else
+                    return Binding.DoNothing;
+            }
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ WPF/WPF_Basic/WpfRadioButton/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WpfRadioButton.FruitEnumToBoolConverter;
+
+namespace WpfRadioButton
+{
+    public class MainViewModel : BindableBase
+    {
+        #region fields, properties
+
+        private FruitType selectedFruit = FruitType.Apple;
+        public FruitType SelectedFruit
+        {
+            get => selectedFruit;
+            set
+            {
+                SetProperty(ref selectedFruit, value);
+            }
+        }
+
+        #endregion
+
+
+        #region constructor
+        public MainViewModel()
+        {
+
+        }
+        #endregion
+    }
+}
