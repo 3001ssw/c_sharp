d53f96c7ae8a40658eadb866606c5fecd61e5b7e
3001ssw <3001ssw@naver.com>
2026-02-12 00:50:45 +0900

    datatemplate add

---
+++ WPF/WPF_Basic/WpfDataTemplate/Chat/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataTemplate.Chat
+{
+    public class ChatMessage : BindableBase
+    {
+        private DateTime time = DateTime.Now;
+        public DateTime Time { get => time; set => SetProperty(ref time, value); }
+
+        private string text = "";
+        public string Text { get => text; set => SetProperty(ref text, value); }
+
+        private bool isMe = false;
+        public bool IsMe { get => isMe; set => SetProperty(ref isMe, value); }
+    }
+}
---
+++ WPF/WPF_Basic/WpfDataTemplate/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDataTemplate.Chat;
+
+namespace WpfDataTemplate
+{
+    public class MainViewModel : BindableBase
+    {
+        private ObservableCollection<ChatMessage> messages = new ObservableCollection<ChatMessage>();
+        public ObservableCollection<ChatMessage> Messages { get => messages; set => SetProperty(ref messages, value); }
+
+
+
+        #region constructor
+        public MainViewModel()
+        {
+            Messages.Add(new ChatMessage()
+            {
+                Text = "hi",
+                IsMe = true,
+            });
+            Messages.Add(new ChatMessage()
+            {
+                Text = "hi",
+                IsMe = false,
+            });
+        }
+        #endregion
+    }
+}
