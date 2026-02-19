6140252bcd622ab90cceba085fd0487eea86df3f
3001ssw <3001ssw@naver.com>
2026-02-19 15:12:05 +0900

    modified database

---
+++ WPF/WPF_Basic/WpfDatabase/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDatabase
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+    }
+
+    public class Grade
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public string ConnectionString { get; set; } = "";
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        // 생성자: 화면에서 선택한 DB 종류와 주소를 받아옵니다.
+        public AppDbContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+    }
+}
--- WPF/WPF_Basic/WpfDatabase/MainViewModel.cs
+++ WPF/WPF_Basic/WpfDatabase/MainViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
@@ -14,29 +14,93 @@ namespace WpfDatabase
     public class MainViewModel : BindableBase
     {
         #region fields, properties
-        private SQLiteConnection _sqlConnection;
+        private AppDbContext db;
 
         private string createDbPath = AppDomain.CurrentDomain.BaseDirectory;
-        public string CreateDbPath { get => createDbPath; set => SetProperty(ref createDbPath, value); }
+        public string CreateDbPath
+        {
+            get => createDbPath;
+            set
+            {
+                SetProperty(ref createDbPath, value);
 
-        private string createDbStatus = "";
-        public string CreateDbStatus { get => createDbStatus; set => SetProperty(ref createDbStatus, value); }
+                ConnectionDbFilePath = Path.Combine(CreateDbPath, CreateDbName);
+            }
+        }
 
+        private string createDbName = "test.db";
+        public string CreateDbName
+        {
+            get => createDbName;
+            set
+            {
+                SetProperty(ref createDbName, value);
+                ConnectionDbFilePath = Path.Combine(CreateDbPath, CreateDbName);
+            }
+        }
+
+        private string status = "";
+        public string Status { get => status; set => SetProperty(ref status, value); }
+
+        private string connectionDbFilePath = "";
+        public string ConnectionDbFilePath { get => connectionDbFilePath; set => SetProperty(ref connectionDbFilePath, value); }
         #endregion
 
         #region commands
 
         public DelegateCommand CreateDbCommand { get; private set; }
+        public DelegateCommand ConnectionDbFilePathCommand { get; private set; }
 
         private void OnCreateDb()
         {
-            SQLiteConnection.CreateFile(CreateDbPath);
-            CreateDbStatus = "Create Database";
+            //string createDbFilePath = Path.Combine(CreateDbPath, CreateDbName);
+            //SqliteConnection.CreateFile(createDbFilePath);
+            //Status = $"Create {CreateDbName}";
         }
 
         private bool CanCreateDb()
         {
             if (string.IsNullOrEmpty(CreateDbPath))
+                return false;
+            if (string.IsNullOrEmpty(CreateDbName))
+                return false;
+
+            return true;
+        }
+
+        private void OnConnectionDbFilePath()
+        {
+            string connectionPath = $"Data Source={ConnectionDbFilePath};";
+            try
+            {
+                db = new AppDbContext(connectionPath);
+                db.Database.EnsureCreated();
+
+                // 4. 데이터 1건 추가 (INSERT)
+                db.Students.Add(new Student { Name = "C# 마스터" });
+
+                // 5. 실제 DB에 쿼리 전송 (COMMIT)
+                db.SaveChanges();
+
+                // 6. 데이터 잘 들어갔나 확인 (SELECT)
+                int count = db.Students.Count();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+            }
+
+            Status = $"Open {CreateDbName}";
+        }
+
+        private bool CanConnectionDbFilePath()
+        {
+            if (string.IsNullOrEmpty(ConnectionDbFilePath))
+                return false;
+            if (db != null)
                 return false;
 
             return true;
@@ -50,6 +114,9 @@ namespace WpfDatabase
         public MainViewModel()
         {
             CreateDbCommand = new DelegateCommand(OnCreateDb, CanCreateDb);
+            ConnectionDbFilePathCommand = new DelegateCommand(OnConnectionDbFilePath, CanConnectionDbFilePath);
+
+            ConnectionDbFilePath = Path.Combine(CreateDbPath, CreateDbName);
         }
         #endregion
     }
