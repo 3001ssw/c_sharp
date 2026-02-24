280f00bc237a55776114ed3b6545180dc57be46b
3001ssw <3001ssw@naver.com>
2026-02-24 13:15:53 +0900

    add database my mssql

--- WPF/WPF_Basic/WpfDatabase/Database/AppDbContext.cs
+++ WPF/WPF_Basic/WpfDatabase/Database/AppDbContext.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -15,6 +17,8 @@ namespace WpfDatabase.Database
     {
         #region fields
         private SqliteConnectionStringBuilder? _sqliteBuilder = null; // sqlite
+        private SqlConnectionStringBuilder? _mssqlBuilder = null; // mssql
+        //private MySqlConnectionStringBuilder? _mysqlBuilder = null; // mysql, mariadb
         #endregion
 
         #region Tables
@@ -29,18 +33,31 @@ namespace WpfDatabase.Database
         {
             // 데이터베이스를 구성하기 위해 메서드 재정의
             if (_sqliteBuilder != null)
-                optionsBuilder.UseSqlite(_sqliteBuilder?.ToString());
+                optionsBuilder.UseSqlite(_sqliteBuilder?.ConnectionString);
+            else if (_mssqlBuilder != null)
+                optionsBuilder.UseSqlServer(_mssqlBuilder?.ConnectionString);
         }
 
         public void ConnectSqlite(string dbFilePath, SqliteOpenMode mode = SqliteOpenMode.ReadWriteCreate)
         {
-            SqliteConnectionStringBuilder sqliteBuilder = new SqliteConnectionStringBuilder();
-            sqliteBuilder.DataSource = dbFilePath;
-            sqliteBuilder.Mode = mode;
-            _sqliteBuilder = sqliteBuilder;
+            SqliteConnectionStringBuilder builder = new SqliteConnectionStringBuilder();
+            builder.DataSource = dbFilePath;
+            builder.Mode = mode;
+            _sqliteBuilder = builder;
 
-            Database.EnsureCreated();
             Students.Load();
+        }
+
+        public void ConnectMssql(string ip, int port, string dbName, string userID, string password)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = $"{ip},{port}";
+            builder.InitialCatalog = dbName;
+            builder.UserID = userID;
+            builder.Password = password;
+            builder.TrustServerCertificate = true;
+            _mssqlBuilder = builder;
         }
 
         public void CloseSqlite()
--- WPF/WPF_Basic/WpfDatabase/MainViewModel.cs
+++ WPF/WPF_Basic/WpfDatabase/MainViewModel.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using WpfDatabase.Database;
 using WpfDatabase.Database.Tables;
@@ -31,6 +32,23 @@ namespace WpfDatabase
 
         private ObservableCollection<Student>? students = null;
         public ObservableCollection<Student>? Students { get => students; set => SetProperty(ref students, value); }
+
+
+        private string mssqlIp = "172.16.5.140";
+        public string MssqlIp { get => mssqlIp; set => SetProperty(ref mssqlIp, value); }
+
+        private int mssqlPort = 1433;
+        public int MssqlPort { get => mssqlPort; set => SetProperty(ref mssqlPort, value); }
+
+        private string mssqlDbName = "wpf_test_db";
+        public string MssqlDbName { get => mssqlDbName; set => SetProperty(ref mssqlDbName, value); }
+
+        private string mssqlID = "acnt";
+        public string MssqlID { get => mssqlID; set => SetProperty(ref mssqlID, value); }
+
+        private string mssqlPassword = "1234";
+        public string MssqlPassword { get => mssqlPassword; set => SetProperty(ref mssqlPassword, value); }
+
         #endregion
 
         #region commands
@@ -38,6 +56,10 @@ namespace WpfDatabase
         public DelegateCommand ConnectDbFilePathCommand { get; private set; }
         public DelegateCommand CloseDbFilePathCommand { get; private set; }
         public DelegateCommand SaveStudentCommand { get; private set; }
+        public DelegateCommand ConnectMssqlCommand { get; private set; }
+
+        public DelegateCommand CloseMssqlCommand { get; private set; }
+
 
         private void OnConnectDbFilePath()
         {
@@ -45,7 +67,7 @@ namespace WpfDatabase
             {
                 DatabaseContext = new AppDbContext();
                 DatabaseContext.ConnectSqlite(DbFilePath);
-
+                DatabaseContext.Database.EnsureCreated();
                 Students = DatabaseContext.Students.Local.ToObservableCollection();
                 Status = "Connect Database";
             }
@@ -103,6 +125,39 @@ namespace WpfDatabase
                 return false;
             return true;
         }
+
+        private async void OnConnectMssql()
+        {
+            try
+            {
+                DatabaseContext = new AppDbContext();
+                DatabaseContext.ConnectMssql(MssqlIp, 1433, MssqlDbName, MssqlID, MssqlPassword);
+                await DatabaseContext.Database.EnsureCreatedAsync();
+                Status = "Connect Database";
+            }
+            catch (Exception e)
+            {
+                Status = "Connect Database Fail" + Environment.NewLine + $"error: {e.Message}";
+            }
+            finally
+            {
+            }
+
+        }
+
+        private bool CanConnectMssql()
+        {
+            return true;
+        }
+        private void OnCloseMssql()
+        {
+
+        }
+
+        private bool CanCloseMssql()
+        {
+            return true;
+        }
         #endregion
 
         #region constructor
@@ -110,9 +165,11 @@ namespace WpfDatabase
         {
             DbFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "School.db");
 
-            ConnectDbFilePathCommand = new DelegateCommand(OnConnectDbFilePath, CanConnectDbFilePath).ObservesProperty(() => DatabaseContext); ;
-            CloseDbFilePathCommand = new DelegateCommand(OnCloseDbFilePath, CanCloseDbFilePath).ObservesProperty(() => DatabaseContext); ;
+            ConnectDbFilePathCommand = new DelegateCommand(OnConnectDbFilePath, CanConnectDbFilePath).ObservesProperty(() => DatabaseContext);
+            CloseDbFilePathCommand = new DelegateCommand(OnCloseDbFilePath, CanCloseDbFilePath).ObservesProperty(() => DatabaseContext);
             SaveStudentCommand = new DelegateCommand(OnSaveStudent, CanSaveStudent).ObservesProperty(() => DatabaseContext);
+            ConnectMssqlCommand = new DelegateCommand(OnConnectMssql, CanConnectMssql);
+            CloseMssqlCommand = new DelegateCommand(OnCloseMssql, CanCloseMssql);
         }
         #endregion
     }
