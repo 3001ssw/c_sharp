3b07a9eeb167ed9ef198a260959bfa454ba48073
3001ssw <3001ssw@naver.com>
2026-04-05 21:19:34 +0900

    mod xUnit

--- xUnit/ConsoleApp1.Tests/UnitTest1.cs
+++ xUnit/ConsoleApp1.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace ConsoleApp1.Tests
 {
     public class UnitTest1
@@ -8,21 +10,16 @@ namespace ConsoleApp1.Tests
             int a = 7;
             int b = 8;
 
-            int result = MathClass.Sum(a, b);
+            int result = MathClass.Sum(a, b) + 1;
 
             Assert.Equal(15, result);
+            //Assert.Equal(16, result);
+            //Assert.True(15 == result);
+            //Assert.False(16 == result);
+
         }
 
-        [Theory]
-        [InlineData(10, 5, 2)]
-        [InlineData(20, 4, 5)]
-        [InlineData(100, 10, 10)]
-        public void DivisionTest(int a, int b, int expected)
-        {
-            Assert.Equal(expected, MathClass.Division(a, b));
-        }
-
-        [Fact]
+        [Fact]  
         public void DivisionRandomTest()
         {
             Random rand = new Random();
@@ -38,9 +35,48 @@ namespace ConsoleApp1.Tests
                 }
                 catch (Exception ex)
                 {
-                    Assert.Fail($"Assert.Fail a: {a}, b: {b} | 원인: {ex.Message}");
+                    Assert.Fail($"Assert.Fail a: {a}, b: {b} | 원인: {ex.Message}"); // 한번 에러나면 끝
                 }
             }
         }
+
+
+        [Theory]
+        [InlineData(10, 5, 2)]
+        [InlineData(20, 4, 5)]
+        [InlineData(100, 10, 10)]
+        [InlineData(0, 0, 0)]
+        public void DivisionTest(int a, int b, int expected)
+        {
+            Assert.Equal(expected, MathClass.Division(a, b));
+        }
+
+
+
+        #region MemberData
+
+        public static IEnumerable<object[]> DivData()
+        {
+            List<object[]> ret = new List<object[]>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int input1 = rand.Next(int.MinValue, int.MaxValue) % 100;
+                int input2 = rand.Next(int.MinValue, int.MaxValue) % 100;
+                object[] obj = new object[] { input1, input2 };
+                ret.Add(obj);
+            }
+            return ret;
+        }
+
+        [Theory]
+        [MemberData(nameof(DivData))]
+        public void DivTest3(int input1, int input2)
+        {
+            int div = MathClass.Division(input1, input2);
+        }
+        #endregion
     }
 }
